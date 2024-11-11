e7bae9a2b92311605adb1ae1ce6a637baa6c0703
MatheusMikio <matheusmikio15@gmail.com>
2024-11-11 20:52:44 -0300

    Atualizado

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ver_produto/', views.ver_produto, name="ver_produto"),
-    # path('inserir_produto/', views.inserir_produto, name="inserir_produto"),
+    path('pesquisa_produto/', views.pesquisa_produto, name="pesquisa_produto"),
     # path('escoher_produto/', views.escolher_produto, name="escolher_produto"),
     # path('visualizar_produto', views.visualizar_produto, name="visualizar_produto")
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -9,19 +9,17 @@ from . import getComentarioMLB
 # def ver_produto(request):
 #     return HttpResponse('Estou no ver')
 
-def ver_produto(request):
+def pesquisa_produto(request):
     if request.method == "GET":
-        nome = 'Caio'
-        return render(request, 'ver_produto.html', {'nome': nome})
+        return render(request, 'index.html')
     
     elif request.method == "POST":
         #ignora 
         nome = request.POST.get('nome')
         listaprod = getComentarioMLB.getMLB(nome)
-        #ignora
-        idade =request.POST.get('idade')
         
-        return HttpResponse(listaprod)
+        
+        return render(request,'teladepesquisa.html',{'produto':listaprod})
 
 # def inserir_produto(request):
 #     return HttpResponse('Estou no inserir')
